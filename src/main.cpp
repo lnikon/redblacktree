a11e1dcabb31fc52a24ea9e2a009c81c850e27c5
nikon <nreplay@list.ru>
2019-06-06 01:05:21 +0400

    insertion fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,11 @@ int main(int argc, char *argv[])
 {
   RBTree rbtree;
   rbtree.insert(11);
-  rbtree.insert(2);
-  rbtree.insert(1);
-  rbtree.insert(7);
-  rbtree.insert(5);
-  rbtree.insert(4);
-  rbtree.insert(8);
-  rbtree.insert(14);
   rbtree.insert(15);
+  rbtree.insert(13);
+  rbtree.insert(14);
+  
+  rbtree.print();
 
   return 0;
 }
--- src/rbtree.hpp
+++ src/rbtree.hpp
@@ -1,38 +1,45 @@
 #pragma once
-#include <stack>
+#include <iostream>
+#include <ostream>
 
 struct RBTree final
 {
-    ~RBTree();
+  ~RBTree();
 
-    enum class Color 
-    {
-        RED     = 0,
-        BLACK   = 1
-    };
+  enum class Color 
+  {
+    RED     = 0,
+    BLACK   = 1
+  };
 
-    struct Node 
-    {
-        Node    *mp_left{nullptr};
-        Node    *mp_right{nullptr};
-        Node    *mp_parent{nullptr};
-        int     m_value{};
-        Color   m_color{Color::BLACK};
-    };
+  struct Node 
+  {
+    Node    *mp_left{nullptr};
+    Node    *mp_right{nullptr};
+    Node    *mp_parent{nullptr};
+    int     m_value{};
+    Color   m_color{Color::BLACK};
+  };
 
-    void leftRotate(Node* pLeft);
-    void rightRotate(Node* pRight);
-    void insert(int value);
-    void transplant(Node* u, Node* v);
+  void    leftRotate(Node* pLeft);
+  void    rightRotate(Node* pRight);
+  Node*   insert(int value);
+  Node*   remove(Node* pNode);
 
-    private:
-    Node* mp_root {nullptr};
+  void print(std::ostream &out = std::cout);
 
-    void leftRotateHelper(Node *pNode, Node* pLeft);
-    void rightRotateHelper(Node *pNode, Node* pRight);
-    void insertHelper(Node* pRoot, Node *pNode);
+  private:
+  Node* mp_root {nullptr};
 
-    void insertFixup(Node* pNode);
+  void leftRotateHelper(Node *pNode, Node* pLeft);
+  void rightRotateHelper(Node *pNode, Node* pRight);
+  void insertHelper(Node* pRoot, Node *pNode);
+  void insertFixup(Node* pNode);
+  Node* removeHelper(Node* pRoot, Node* pNode);
+  Node* successor(Node *pNode);
+  void  removeFixup(Node* pRoot, Node* pNode);
+
+  void printHelper(Node* pNode, std::ostream &out = std::cout);
 };
 
 RBTree::~RBTree()
@@ -41,132 +48,302 @@ RBTree::~RBTree()
 
 void RBTree::leftRotate(Node* pLeft)
 { 
-    leftRotateHelper(mp_root, pLeft);
+  leftRotateHelper(mp_root, pLeft);
 }
 
 void RBTree::rightRotate(Node* pRight)
 { 
-    rightRotateHelper(mp_root, pRight);
+  rightRotateHelper(mp_root, pRight);
 }
 
-void RBTree::insert(int value) 
+RBTree::Node* RBTree::insert(int value) 
 { 
-    Node *pNode = new Node;
-    pNode->m_value = value;
+  Node *pNode = new Node;
+  pNode->m_value = value;
 
-    insertHelper(mp_root, pNode);
+  insertHelper(mp_root, pNode);
+
+  return pNode;
 }
 
-void RBTree::transplant(Node* u, Node* v)
+RBTree::Node* RBTree::remove(Node* pNode)
 {
-    if(u->mp_parent == nullptr) {
-        mp_root = v;
-    } else if(u == u->mp_parent->mp_left) {
-        u->mp_parent->mp_left = v;
-    } else {
-        u->mp_parent->mp_right = v;
-    }
+  return removeHelper(mp_root, pNode);
+}
 
-    v->mp_parent = u->mp_parent;
+void RBTree::print(std::ostream &out)
+{
+  if(mp_root == nullptr)
+  {
+    return;
+  }
+
+  printHelper(mp_root, out);
+}
+
+RBTree::Node* RBTree::removeHelper(Node* pRoot, Node* pNode)
+{
+  Node* yNode = nullptr;
+  if(pNode->mp_left == nullptr 
+      || pNode->mp_right == nullptr)
+  {
+    yNode = pNode;
+  }
+  else
+  {
+    yNode = successor(pNode); 
+  }
+
+  Node* xNode = nullptr;
+  if(yNode->mp_left != nullptr)
+  {
+    xNode = yNode->mp_left;
+  } 
+  else
+  {
+    xNode = yNode->mp_right;
+  }
+
+  xNode->mp_parent = yNode->mp_parent;
+
+  if(yNode->mp_parent == nullptr)
+  {
+    mp_root = xNode;
+  }
+  else if(yNode == yNode->mp_parent->mp_left)
+  {
+    yNode->mp_parent->mp_left = xNode;
+  }
+  else
+  {
+    yNode->mp_parent->mp_right = xNode;
+  }
+
+  if(yNode != pNode)
+  {
+    pNode->m_value = yNode->m_value;
+  }
+
+  if(yNode->m_color == Color::BLACK)
+  {
+    removeFixup(pRoot, pNode);
+  }
+
+  return yNode;
+}
+
+RBTree::Node* RBTree::successor(Node *pNode)
+{
+  Node* pTemp = pNode;
+
+  while(pTemp->mp_left != nullptr) {
+    pTemp = pTemp->mp_left;
+  }
+
+  return pTemp;
+}
+
+void RBTree::removeFixup(Node* pRoot, Node* pNode)
+{
+  while(pNode != pRoot && pNode->m_color == Color::BLACK)
+  {
+    Node* wNode = nullptr;
+    if(pNode == pNode->mp_parent->mp_right)
+    {
+      wNode = pNode->mp_parent->mp_right;
+      if(wNode->m_color == Color::RED)
+      {
+        wNode->m_color = Color::BLACK;
+        wNode->mp_parent->m_color = Color::RED;
+        leftRotate(pNode->mp_parent);
+        wNode = pNode->mp_parent->mp_right;
+      }
+
+      if(wNode->mp_left->m_color == Color::BLACK 
+          && wNode->mp_right->m_color == Color::BLACK)
+      {
+        wNode->m_color = Color::RED;
+        pNode = pNode->mp_parent;
+      }
+      else if(wNode->mp_right->m_color == Color::BLACK)
+      {
+        wNode->mp_left->m_color = Color::BLACK;
+        wNode->m_color = Color::RED;
+        rightRotate(wNode);
+        wNode = pNode->mp_parent->mp_right;
+      }
+
+      wNode->m_color = pNode->mp_parent->m_color;
+      pNode->mp_parent->m_color = Color::BLACK;
+      wNode->mp_right->m_color = Color::BLACK;
+      leftRotate(pNode->mp_parent);
+      pNode = pRoot;
+    } 
+    else
+    {
+      wNode = pNode->mp_parent->mp_left;
+      if(wNode->m_color == Color::RED)
+      {
+        wNode->m_color = Color::BLACK;
+        wNode->mp_parent->m_color = Color::RED;
+        rightRotate(pNode->mp_parent);
+        wNode = pNode->mp_parent->mp_left;
+      }
+
+      if(wNode->mp_right->m_color == Color::BLACK 
+          && wNode->mp_left->m_color == Color::BLACK)
+      {
+        wNode->m_color = Color::RED;
+        pNode = pNode->mp_parent;
+      }
+      else if(wNode->mp_left->m_color == Color::BLACK)
+      {
+        wNode->mp_right->m_color = Color::BLACK;
+        wNode->m_color = Color::RED;
+        leftRotate(wNode);
+        wNode = pNode->mp_parent->mp_left;
+      }
+
+      wNode->m_color = pNode->mp_parent->m_color;
+      pNode->mp_parent->m_color = Color::BLACK;
+      wNode->mp_left->m_color = Color::BLACK;
+      rightRotate(pNode->mp_parent);
+      pNode = pRoot;
+    }
+  }
+
+  pNode->m_color = Color::BLACK;
+}
+
+void RBTree::printHelper(Node* pNode, std::ostream &out)
+{
+  if(pNode == nullptr)
+  {
+    return;
+  }
+
+  out << pNode->m_value << ' ';
+  printHelper(pNode->mp_left);
+  printHelper(pNode->mp_right);
 }
 
 void RBTree::leftRotateHelper(Node *pRoot, Node* pNode)
 { 
-    Node* rightNode = pNode->mp_right;
-    pNode->mp_right = rightNode->mp_left;
+  Node* rightNode = pNode->mp_right;
+  pNode->mp_right = rightNode->mp_left;
 
-    if(rightNode->mp_right != nullptr) {
-        rightNode->mp_right->mp_parent = pNode;
-    }
+  if(rightNode->mp_right != nullptr) {
+    rightNode->mp_right->mp_parent = pNode;
+  }
 
-    rightNode->mp_parent = pNode->mp_parent;
-    if(pNode->mp_parent == nullptr) {
-        pRoot = rightNode;
-    } else if(pNode == pNode->mp_parent->mp_left) {
-        pNode->mp_parent->mp_left = rightNode;
-    } else {
-        pNode->mp_parent->mp_right = rightNode;
-    }
+  rightNode->mp_parent = pNode->mp_parent;
+  if(pNode->mp_parent == nullptr) {
+    pRoot = rightNode;
+  } else if(pNode == pNode->mp_parent->mp_left) {
+    pNode->mp_parent->mp_left = rightNode;
+  } else {
+    pNode->mp_parent->mp_right = rightNode;
+  }
 
-    rightNode->mp_left = pNode;
+  rightNode->mp_left = pNode;
 
-    pNode->mp_parent = rightNode;
+  pNode->mp_parent = rightNode;
 }
 
 void RBTree::rightRotateHelper(Node *pRoot, Node* pNode)
 { 
-    Node *pLeft = pNode->mp_left; 
-    pNode->mp_left = pLeft->mp_right;
+  Node *pLeft = pNode->mp_left; 
+  pNode->mp_left = pLeft->mp_right;
 
-    if(pNode->mp_left != nullptr) {
-        pNode->mp_left->mp_parent = pNode;
-    }
+  if(pNode->mp_left != nullptr) {
+    pNode->mp_left->mp_parent = pNode;
+  }
 
-    pLeft->mp_parent = pNode->mp_parent;
+  pLeft->mp_parent = pNode->mp_parent;
 
-    if(pNode->mp_parent == nullptr) {
-        pRoot = pLeft;
-    } else if(pNode == pNode->mp_parent->mp_left) {
-        pNode->mp_parent->mp_left = pLeft;
-    } else {
-        pNode->mp_parent->mp_right = pLeft;
-    }
+  if(pNode->mp_parent == nullptr) {
+    pRoot = pLeft;
+  } else if(pNode == pNode->mp_parent->mp_left) {
+    pNode->mp_parent->mp_left = pLeft;
+  } else {
+    pNode->mp_parent->mp_right = pLeft;
+  }
 
-    pLeft->mp_right = pNode;
-    pNode->mp_parent = pLeft;
+  pLeft->mp_right = pNode;
+  pNode->mp_parent = pLeft;
 }
 
 void RBTree::insertHelper(Node* pRoot, Node *pNode)
 { 
-    Node* y = nullptr;
-    Node* x = pRoot;
+  Node* y = nullptr;
+  Node* x = pRoot;
 
-    // Find place to insert
-    while(x != nullptr) {
-        y = x;
-        if(pNode->m_value < x->m_value) {
-            x = x->mp_left;
-        } else {
-            x = x->mp_right;
-        }
+  // Find place to insert
+  while(x != nullptr) {
+    y = x;
+    if(pNode->m_value < x->m_value) {
+      x = x->mp_left;
+    } else {
+      x = x->mp_right;
     }
+  }
 
-    // Do insertion
-    pNode->mp_parent = y;
-    if(y == nullptr) {
-        mp_root = pNode;
+  // Do Insertion
+  pNode->mp_parent = y;
+
+  pNode->mp_left = nullptr;
+  pNode->mp_right = nullptr;
+  pNode->m_color = Color::RED;
+
+  if(y == nullptr) {
+    mp_root = pNode;
+    return;
   } else if(pNode->m_value < y->m_value) {
     y->mp_left = pNode;
   } else {
     y->mp_right = pNode;
   }
 
-  pNode->mp_left = nullptr;
-  pNode->mp_right = nullptr;
-  pNode->m_color = Color::RED;
-
   insertFixup(pNode);
 }
 
 void RBTree::insertFixup(Node* pNode)
 {
+  if(pNode == mp_root)
+  {
+    pNode->m_color = Color::BLACK;
+    return;
+  }
+
   Node* parent = nullptr;
   Node* grandParent = nullptr;
 
-  while(pNode != mp_root && pNode->m_color == Color::RED && pNode->mp_parent->m_color == Color::RED) {
+  std::cout << "\nDEBUG[\n";
+  std::cout << "pNode:" << pNode << std::endl;
+  std::cout << "mp_root: " << mp_root << std::endl;
+  std::cout << "pNode->mp_parent: " << pNode->mp_parent << std::endl;
+  std::cout << "pNode->mp_parent->m_color: " << (int)pNode->mp_parent->m_color << std::endl;
+  std::cout << "\nUNDEBUG[\n";
+  while(pNode->m_color == Color::RED && pNode->mp_parent->m_color == Color::RED) {
     parent = pNode->mp_parent;
     grandParent = parent->mp_parent;
 
-    if(grandParent != nullptr && parent == grandParent->mp_left) {
+    if(grandParent != nullptr && parent == grandParent->mp_left) 
+    {
+      std::cout << "in left branch\n";
       Node* otherNode = grandParent->mp_right;
 
-      if(otherNode != nullptr && otherNode->m_color == Color::RED) {
+      if(otherNode != nullptr && otherNode->m_color == Color::RED) 
+      {
         pNode->mp_parent->m_color = Color::BLACK;
         otherNode->m_color = Color::BLACK;
         pNode->mp_parent->mp_parent->m_color = Color::RED;
         pNode = pNode->mp_parent->mp_parent;
-      } else { 
-        if(pNode == pNode->mp_parent->mp_right) {
+      } 
+      else 
+      { 
+        if(pNode == pNode->mp_parent->mp_right) 
+        {
           pNode = pNode->mp_parent;
           leftRotate(pNode);
         }
@@ -175,8 +352,11 @@ void RBTree::insertFixup(Node* pNode)
         pNode->mp_parent->mp_parent->m_color = Color::RED;
         rightRotate(pNode->mp_parent->mp_parent);
       }
-    } else {
-      Node* otherNode = pNode->mp_parent->mp_parent->mp_left;
+    } 
+    else if(grandParent != nullptr)
+    {
+      std::cout << "in right branch\n";
+      Node* otherNode = grandParent->mp_left;
       if(otherNode != nullptr && otherNode->m_color == Color::RED) {
         pNode->mp_parent->m_color = Color::BLACK;
         otherNode->m_color = Color::BLACK;
